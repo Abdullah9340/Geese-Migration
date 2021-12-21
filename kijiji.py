0cd387d2cbf28b60f70b77a9228cae9b84e80f4f
Abdullah Abdullah <a55abdul@uwaterloo.ca>
2021-12-21 18:21:45 -0500

    Initial Commit. Added kijiji web scraper
    Co-authored-by: Evan Guo <0ku@users.noreply.github.com>

---
+++ kijiji.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+from main import House
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
+url = "https://www.kijiji.ca/b-real-estate/kitchener-waterloo/student-housing/k0c34l1700212?rb=true&ll=43.464258%2C-80.520410&address=Waterloo%2C+ON&ad=offering&radius=50.0"
+page = requests.get(url, headers=headers)
+soup = BeautifulSoup(page.content, "html.parser")
+
+
+def get_kijiji_listings():
+    houses = []
+    for link in soup.find_all('div', {'class': 'clearfix'}):
+        if link.find('div', {'class': 'price'}):
+            posting_name = str(
+                link.find('a', {'class': 'title'}, href=True).get_text()).strip().replace("    ", "")
+            posting_price = str(
+                link.find('div', {'class': 'price'}).get_text()).strip()
+            posting_link = f"https://www.kijiji.ca{link.find('a', href=True).get('href')}".strip(
+            )
+            posting_desc = str(
+                link.find('div', {'class': 'description'}).get_text()).strip().replace("    ", "")
+
+            if link.find('span', {'class': 'bedrooms'}):
+                posting_beds = str(
+                    link.find('span', {'class': 'bedrooms'}).get_text()).strip().replace("    ", "")
+            else:
+                posting_beds = ""
+            if link.find('span', {'class': 'intersection'}):
+                posting_location = str(
+                    link.find('span', {'class': 'intersection'}).get_text()).strip().replace("    ", "")
+            else:
+                posting_location = ""
+
+            houses.append(House(posting_name, posting_price,
+                          posting_desc, posting_beds, posting_location, posting_link, "image"))
+    return houses
+
+
+houses = get_kijiji_listings()
+
+for a in houses:
+    print("--------------------------------------------")
+    print(a)
+    print("--------------------------------------------")
---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class House:
+    def __init__(self, listing_name, price, desc, beds, location, link, image):
+        self.listing_name = listing_name
+        self.price = price
+        self.desc = desc
+        self.beds = beds
+        self.location = location
+        self.link = link
+        self.image = image
+
+    def __str__(self):
+        a = f"{self.listing_name} {self.price} {self.beds} {self.location}\n{self.desc}"
+        return a
+
+
+kijiji_listings = []
+
+houses = []
