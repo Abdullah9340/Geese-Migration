67fa562a2c094dbe7d9e7ee340df8bfdb74b644d
Abdullah Abdullah <a55abdul@uwaterloo.ca>
2021-12-27 18:09:40 -0500

    adding extra

--- houses.py
+++ houses.py
@@ -1,7 +1,7 @@
 
 
 class House:
-    def __init__(self, listing_name, price, desc, beds, location, link, image):
+    def __init__(self, listing_name, price, desc, beds, location, link, image, id=-1):
         self.listing_name = listing_name
         self.price = price
         self.desc = desc
@@ -9,6 +9,7 @@ class House:
         self.location = location
         self.link = link
         self.image = image
+        self.id = id
 
     def __str__(self):
         a = f"{self.listing_name} {self.price} {self.beds} {self.location}\n{self.desc}"
--- sqlsetup.py
+++ sqlsetup.py
@@ -8,11 +8,13 @@ db = mysql.connector.connect(
 )
 
 mycursor = db.cursor()
-mycursor.execute("SHOW COLUMNS FROM housedb")
+#mycursor.execute("SHOW COLUMNS FROM housedb")
+mycursor.execute(
+    "INSERT INTO housedb (listing_name, price, location, description, contact, image) VALUES (%s,12,43,5,4,3)", ("tjhaks",))
 # mycursor.execute(
 #    "CREATE TABLE HouseDB (id int PRIMARY KEY AUTO_INCREMENT, listing_name varchar(255) NOT NULL, " +
 #    "price varchar(255) NOT NULL, location varchar(255) NOT NULL, description varchar(1000) NOT NULL, " +
 #    "contact varchar(255) NOT NULL, image varchar(500))" +
 #    ")")
 for x in mycursor:
-    print(x)
+    print(type(x[0]))
