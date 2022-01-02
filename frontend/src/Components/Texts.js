cef8b5370a3a9df72918aa8aeea4a0c3134f0438
Abdullah Abdullah <a55abdul@uwaterloo.ca>
2022-01-02 14:00:55 -0500

    Created the start of react frontend
    Co-authored-by: Evan Guo <0ku@users.noreply.github.com>

---
+++ frontend/src/Components/Texts.js
@@ -0,0 +1,10 @@
+import CardHeader from "../Components";
+
+function CardHeader(props) {
+  <div className="CardHeader">{props.text}</div>;
+}
+
+function StandardText1(props) {
+  <div className="StandardText1"></div>
+}
+export default CardHeader;
